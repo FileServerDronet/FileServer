374e00ecf47e3419d8dccf2f7243b78bbe195d06
Selin <141830235+seliin21@users.noreply.github.com>
2023-09-20 10:10:59 +0300

    ss

--- dist/main.d.ts
+++
@@ -1,4 +0,0 @@
-import { Response } from 'express';
-export declare class AppController {
-    getHomePage(res: Response): void;
-}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { Script } from 'vm';
 
 @Module({
-  imports: [
-    
-  ]
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
-
