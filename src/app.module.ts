67d6085b579a4e57d11ee7adf83a7748699ace38
Selin <141830235+seliin21@users.noreply.github.com>
2023-09-20 10:59:20 +0300

    ss

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { FileUploadService } from './file-upload/file-upload.service';
+import { FileUploadController } from './file-upload/file-upload.controller';
+import { FileDeleteService } from './file-delete/file-delete.service';
+import { FileDeleteController } from './file-delete/file-delete.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, FileUploadController, FileDeleteController],
+  providers: [AppService, FileUploadService, FileDeleteService],
 })
 export class AppModule {}
