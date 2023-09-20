0a160ce7e802d89c82e4d5f7faf6431137afa81f
Selin <141830235+seliin21@users.noreply.github.com>
2023-09-20 15:02:33 +0300

    a

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,16 +1,12 @@
-import { Body, Controller, Get, Post, Res, UploadedFile, UseInterceptors, Delete } from "@nestjs/common";
-import {FileInterceptor} from "@nestjs/platform-express";
-import {diskStorage} from "multer";
-import {Response} from "express";
-import  * as path from "path";
+import { Body, Controller, Get, Post, Res, UploadedFile, UseInterceptors, Delete, Query } from "@nestjs/common";
+import { FileInterceptor } from "@nestjs/platform-express";
+import { diskStorage } from "multer";
+import { Response } from "express";
+import * as path from "path";
 import * as fs from 'fs';
-<<<<<<< Updated upstream
-
-=======
->>>>>>> Stashed changes
 
 interface FileParams {
-  fileName : string;
+  fileName: string;
 }
 
 @Controller()
@@ -21,68 +17,41 @@ export class AppController {
     return "Hello World";
   }
 
-
   @Post("/upload")
-  @UseInterceptors(FileInterceptor('file' , {
-    storage : diskStorage({
-      destination : "./uploads",
-      filename : (req , file , cb) => {
-        cb(null , `${file.originalname}`)
+  @UseInterceptors(FileInterceptor('file', {
+    storage: diskStorage({
+      destination: "./uploads",
+      filename: (req, file, cb) => {
+        cb(null, `${file.originalname}`)
       }
     })
   }))
-  async uploadFile(@UploadedFile() file : any) {
+  async uploadFile(@UploadedFile() file: any) {
     console.log(file);
     return "success";
   }
 
-
   @Get("/getFile")
-  getFile(@Res() res : Response , @Body() file : FileParams)
-  {
-    res.sendFile(path.join(__dirname , "./uploads/" + file.fileName));
+  getFile(@Res() res: Response, @Query('fileName') fileName: string) {
+    const filePath = path.join(__dirname, "./uploads/", fileName);
+    res.sendFile(filePath);
   }
 
   @Delete("/deleteFile")
-<<<<<<< Updated upstream
-  async deleteFile(@Res() res: Response, @Body() file: FileParams) {
+  async deleteFile(@Res() res: Response, @Query('fileName') fileName: string) {
     try {
-      const filePath = path.join(__dirname, "./uploads/" + file.fileName);
-=======
-async deleteFile(@Res() res: Response, @Body() file: FileParams) {
-  try {
-    const filePath = path.join(__dirname, "./uploads/" + file.fileName);
-
-    // Dosya varsa sil
-    if (fs.existsSync(filePath)) {
-      fs.unlinkSync(filePath);
-      return res.status(200).json({ message: "Dosya başarıyla silindi." });
-    } else {
-      return res.status(404).json({ message: "Dosya bulunamadı." });
-    }
-  } catch (error) {
-    console.error("Dosya silinirken hata oluştu:", error);
-    return res.status(500).json({ message: "Dosya silinirken bir hata oluştu." });
-  }
-}
-
->>>>>>> Stashed changes
-
-      console.log("Dosya Yolu:", filePath); // Dosya yolunu konsola yazdır
+      const filePath = path.join(__dirname, "./uploads/", fileName);
 
       // Dosya varsa sil
       if (fs.existsSync(filePath)) {
         fs.unlinkSync(filePath);
         return res.status(200).json({ message: "Dosya başarıyla silindi." });
       } else {
-        console.log("Dosya Bulunamadı"); // Dosyanın bulunamadığını konsola yazdır
         return res.status(404).json({ message: "Dosya bulunamadı." });
       }
     } catch (error) {
-      
       console.error("Dosya silinirken hata oluştu:", error);
       return res.status(500).json({ message: "Dosya silinirken bir hata oluştu." });
     }
   }
-  
 }
